4a7d607b7e49ad6755fff2e6e56e058e2dce1447
Ben Botsford <ben.botsford@flatironschool.com>
2022-10-25 15:35:30 -0500

    add solution

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,11 @@
 def write_file(file_name, file_content):
-    pass
+    with open(file_name, 'w') as f:
+        f.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(file_name, 'a') as f:
+        f.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(file_name, 'r') as f:
+        return f.read()
