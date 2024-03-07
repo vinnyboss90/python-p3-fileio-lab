379d61784d96afdb97c1bb9b53197b10b54a823c
vinnyegon90 <vinnyegon90@gmail.com>
2024-03-07 12:46:15 +0300

    Done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,12 @@
 def write_file(file_name, file_content):
-    pass
+    t = open(str(file_name) + '.txt', 'w')
+    t.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    t = open(str(file_name) + '.txt', 'a')
+    t.write(append_content)
 
 def read_file(file_name):
-    pass
+    t = open(str(file_name) + '.txt', 'r')
+    content = t.read()
+    return content
